948ae9a7226c73ba0cb6ba52a139d249dc6e7921
Ligh7bringer <sfe7lio@gmail.com>
2017-03-14 20:16:08 +0000

    Implemented basic game loop

---
+++ app/src/main/java/sgeorgiev/org/androidcoursework/MainThread.java
@@ -0,0 +1,77 @@
+package sgeorgiev.org.androidcoursework;
+
+import android.graphics.Canvas;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by Svetlozar Georgiev on 14/03/2017.
+ */
+
+public class MainThread extends Thread {
+    public static final int MAX_FPS = 30;
+    private double avgFPS;
+    private SurfaceHolder surfaceHolder;
+    private GamePanel gamePanel;
+    private boolean running;
+    public static Canvas canvas;
+
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+
+    public void setRunning(boolean value) {
+        this.running = value;
+    }
+
+    @Override
+    public void run() {
+        long startTime;
+        long timeMillis = 1000/MAX_FPS;
+        long waitTime;
+        long frameCount = 0;
+        long totalTime = 0;
+        long targetTime = 1000/MAX_FPS;
+
+        while(running) {
+            startTime = System.nanoTime();
+            canvas = null;
+
+            try {
+                canvas = surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    this.gamePanel.update();
+                    this.gamePanel.draw(canvas);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e) { e.printStackTrace(); }
+                }
+            }
+            timeMillis = (System.nanoTime() - startTime)/1000000;
+            waitTime = targetTime - timeMillis;
+            try {
+                if(waitTime > 0)
+                    this.sleep(waitTime);
+            } catch (Exception e) { e.printStackTrace(); }
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+
+            if(frameCount == MAX_FPS) {
+                avgFPS = 1000/((totalTime/frameCount)/1000000);
+                frameCount = 0;
+                totalTime = 0;
+                Log.d("FPS: ", String.valueOf(avgFPS));
+            }
+        }
+    }
+
+}
